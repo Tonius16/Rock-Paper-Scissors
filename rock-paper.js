23d10bc67b945b3d8160d75b48889ab5a6a87a89
Toni <toni.cartman@gmail.com>
2023-03-16 12:53:24 +0100

    add imgs and basic html skeleton

--- rock-paper.js
+++ rock-paper.js
@@ -11,88 +11,65 @@
 
 const choices = ["rock", "paper", "scissors"];
 
-  function getComputerChoice() {
-    let cpuChoice = choices [Math.floor(Math.random() * choices.length)];
-    return cpuChoice;
+function getComputerChoice() {
+  let cpuChoice = choices[Math.floor(Math.random() * choices.length)];
+  return cpuChoice;
 }
 
 function playRound(playerSelection, cpuSelection) {
-    if (playerSelection === "scissors" && cpuSelection === "rock" ) {
-        return "you loose";
-     }
-     else if (playerSelection === "scissors" && cpuSelection === "paper" ) {
-         return "you win";
-      }
-      else if (playerSelection === "scissors" && cpuSelection === "scissors" ) {
-         return "draw";
-      }
-      else if (playerSelection === "rock" && cpuSelection === "paper" ) {
-        return "you loose";
-      }
-      else if (playerSelection === "rock" && cpuSelection === "scissors" ) {
-        return "you win";
-      }
-      else if (playerSelection === "rock" && cpuSelection === "rock" ) {
-        return "draw";
-      }
-      else if (playerSelection === "paper" && cpuSelection === "scissors" ) {
-        return "you loose";
-      }
-      else if (playerSelection === "paper" && cpuSelection === "rock" ) {
-        return "you win";
-      }
-      else if (playerSelection === "paper" && cpuSelection === "paper" ) {
-        return "draw";
-      }
-      else if (playerSelection != "") {
-        return "wrong value"
-      }
-    }
-    
+  if (playerSelection === "scissors" && cpuSelection === "rock") {
+    return "you lose";
+  } else if (playerSelection === "scissors" && cpuSelection === "paper") {
+    return "you win";
+  } else if (playerSelection === "scissors" && cpuSelection === "scissors") {
+    return "draw";
+  } else if (playerSelection === "rock" && cpuSelection === "paper") {
+    return "you lose";
+  } else if (playerSelection === "rock" && cpuSelection === "scissors") {
+    return "you win";
+  } else if (playerSelection === "rock" && cpuSelection === "rock") {
+    return "draw";
+  } else if (playerSelection === "paper" && cpuSelection === "scissors") {
+    return "you lose";
+  } else if (playerSelection === "paper" && cpuSelection === "rock") {
+    return "you win";
+  } else if (playerSelection === "paper" && cpuSelection === "paper") {
+    return "draw";
+  } else {
+    return "wrong value";
+  }
+}
+
 function game() {
+  let playerScore = 0;
+  let cpuScore = 0;
 
-    let playerScore = 0;
-    let cpuScore = 0;
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("rock paper scissors go").toLowerCase();
+    let cpuSelection = getComputerChoice();
+    let result = playRound(playerSelection, cpuSelection);
 
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("rock paper scissors go").toLowerCase()
-        let cpuSelection = getComputerChoice();
-        let result = playRound(playerSelection, cpuSelection);
-        
-        if (result === "you loose") {
-           cpuScore += 1;
-           console.log(playerScore, cpuScore)
-        }  
-         else if (result === "you win") {
-           playerScore += 1;
-           console.log(playerScore, cpuScore)
-        }
-         else if (result === "draw") {
-           i -= 1;
-           console.log("draw", playerScore, cpuScore)
-        }
-        else if (result === "wrong value") {
-           i -= 1
-           console.log("wrong value")
-        }
-
+    if (result === "you lose") {
+      cpuScore += 1;
+      console.log(playerScore, cpuScore);
+    } else if (result === "you win") {
+      playerScore += 1;
+      console.log(playerScore, cpuScore);
+    } else if (result === "draw") {
+      i -= 1;
+      console.log("draw", playerScore, cpuScore);
+    } else if (result === "wrong value") {
+      i -= 1;
+      console.log("wrong value");
     }
-    let winner;
-    if (playerScore < cpuScore) {
-      winner = "cpu";
-    }
-     else if (playerScore > cpuScore) {
-      winner = "player";
-     }
-     return winner;
-    }
+  }
+  let winner;
+  if (playerScore < cpuScore) {
+    winner = "cpu";
+  } else if (playerScore > cpuScore) {
+    winner = "player";
+  }
+  return winner;
+}
 
-  
-
-    
-
-
-
- 
-
-
+console.log(game());
